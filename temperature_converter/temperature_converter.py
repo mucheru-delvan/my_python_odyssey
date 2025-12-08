61b4f40900b79e82bcc7db42c0e0dfa0f296ef85
mucheru-delvan <mkdelvan9@gmail.com>
2025-12-08 23:14:48 +0300

    Updated the temperature converter

--- temperature_converter/temperature_converter.py
+++ temperature_converter/temperature_converter.py
@@ -2,7 +2,7 @@
 def temperature_converter():
 
     while True:
-        print("\n🌟Temperature Converter🌟")
+        print("\n🌟Temperature Converter🌟\n")
         print("1.Celsius to Fahreinheit")
         print("2.Celsius to Kelvin")
         print("3.Fahreinheit to Kelvin")
@@ -20,12 +20,12 @@ def temperature_converter():
                 print(f"{temp} Fahreinheit = {(temp - 32)*5/9:.2f} Celsius")
             elif option == "2":
                 temp = float(input("Enter temperature: "))
-                print(f"{temp} Celsius = {(temp + 273)} Kelvin")
-                print(f"{temp} Kelvin = {temp - 273} Celsius")
+                print(f"{temp} Celsius = {(temp + 273.15):.2f} Kelvin")
+                print(f"{temp} Kelvin = {(temp - 273.15):.2f} Celsius")
             elif option == "3":
                 temp = float(input("Enter temperature: "))
                 print(f"{temp} Fahreinheit = {(temp - 32) * 5/9 + 273:.2f} Kelvin")
-                print(f"{temp} Kelvin = {(temp - 273) * 9/5 + 32:.2f} Fahreinheit")
+                print(f"{temp} Kelvin = {(temp - 273.15) * 9/5 + 32:.2f} Fahreinheit")
 
         except ValueError:
             print("Please enter a valid number!")
