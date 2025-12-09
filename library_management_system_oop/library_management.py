ea15ed747ac349e1eade784ee32edc158770292f
mucheru-delvan <mkdelvan9@gmail.com>
2025-12-09 21:21:27 +0300

    Added new .py file for the library project

--- library_management_system_oop/library_management.py
+++ library_management_system_oop/library_management.py
@@ -6,3 +6,56 @@ class Library:
     def add_book(self,book):
         self.books.append(book)
         print(f"Added: {book.title}")
+
+    def add_user(self,user):
+        self.users.append(user)
+        print(f"Added user: {user.name}")
+
+    def find_book(self,title):
+        for book in self.books:
+            if book.title.lower() == title.lower():
+                return book
+        return None
+    
+    def find_user(self,user_id):
+        for user in self.users:
+            if user.user_id == user_id:
+                return user
+        return None
+    
+    def show_all_books(self):
+        print("\nAll books in library:")
+        for book in self.books:
+            print(f"-{book}")
+
+    def show_all_users(self):
+        print("\nAll Users:")
+        for user in self.users:
+            borrowed = len(user.borrowed_books)
+            print(f" - {user.name} (ID: {user.user_id}) - Borrowed: {borrowed} books")
+
+    def borrow_book(self,user_id,book_title):
+        user = self.find_user(user_id)
+        book = self.find_book(book_title)
+
+        if user and book:
+            return user.borrow_book(book)
+        elif not user:
+            print(f"User {user_id} not found!")
+        elif not book:
+            print(f"Book '{book_title}' not found!")
+        return False
+    
+    def return_book(self,user_id,book_title):
+        user = self.find_user(user_id)
+        book = self.find_book(book_title)
+
+        if user and book:
+            return user.return_book(book)
+        elif not user:
+            print(f"User {user_id} not found!")
+        elif not book:
+            print(f"Book '{book_title}' not found!")
+        return False
+            
+    
--- library_management_system_oop/main.py
+++ library_management_system_oop/main.py
@@ -1,14 +1,73 @@
 from book import Book
 from user import User
+from library_management import Library
+
+library = Library()
+
 
 book1 = Book("1984", "George Orwell", "1234567890")
 book2 = Book("To Kill a Mockingbird", "Harper Lee", "0987654321")
 book3 = Book("The Great Gatsby", "F. Scott Fitzgerald", "1122334455")
+book4 = Book("Harry Potter", "J.K. Rowling", "2233445566")
+book5 = Book("The Hobbit", "J.R.R. Tolkien", "3344556677")
+book6 = Book("Pride and Prejudice", "Jane Austen", "4455667788")
+book7 = Book("The Catcher in the Rye", "J.D. Salinger", "5566778899")   
+book8 = Book("The Lord of the Rings", "J.R.R. Tolkien", "6677889900")
+book9 = Book("Animal Farm", "George Orwell", "7788990011")
+book10 = Book("Moby Dick", "Herman Melville", "8899001122") 
+book11 = Book("War and Peace", "Leo Tolstoy", "9900112233")
+book12 = Book("The Odyssey", "Homer", "1011121314") 
+book13 = Book("Crime and Punishment", "Fyodor Dostoevsky", "1213141516")
+book14 = Book("The Divine Comedy", "Dante Alighieri", "1314151617")
+book15 = Book("Brave New World", "Aldous Huxley", "1415161718")
 #print(book1)
 
-user1 = User("Alice", "U001")
-user1.borrow_book(book1)
-user1.borrow_book(book2)
-user1.return_book(book1)
-user1.borrow_book(book3)
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+library.add_book(book4)
+library.add_book(book5)
+library.add_book(book6)
+library.add_book(book7)
+library.add_book(book8)
+library.add_book(book9)
+library.add_book(book10)
+library.add_book(book11)
+library.add_book(book12)
+library.add_book(book13)
+library.add_book(book14)
+library.add_book(book15)    
+
+
+user1 = User("Alice","U001")
+user2 = User("Melvin","U002")
+user3 = User("Charlie","U003")
+user4 = User("Diana","U004")
+user5 = User("Eve","U005")
+
+
+
+
+library.add_user(user1)
+library.add_user(user2)
+
+
+library.borrow_book("U002","The Great Gatsby") 
+
+library.return_book("U002","The Great Gatsby")
+
+library.borrow_book("U003","To Kill a Mockingbird")
+
+library.add_user(user3)
+library.add_user(user4)
+library.add_user(user5) 
+
+
+library.show_all_books()
+library.show_all_users()
+
+library.borrow_book("U001","1984")
+library.borrow_book("U001","The Hobbit")
+library.borrow_book("U001","Moby Dick")
 user1.show_books()
+
