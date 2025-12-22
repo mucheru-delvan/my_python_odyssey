acf67e808e7678af2f9b182a3bc5a8ece07f529a
mucheru-delvan <mkdelvan9@gmail.com>
2025-12-22 08:38:16 +0300

    Updated the product.py

--- restaurant_ordering_system_oop/product.py
+++ restaurant_ordering_system_oop/product.py
@@ -1,13 +1,10 @@
 class Product:
-    def __init__(self,name,price,quantity):
+    def __init__(self,name,price):
         self.name = name
         self.price = price
-        self.quantity = quantity
-
-
+        
     def __str__(self):
-        return f"{self.name} (x{self.quantity}) - ${self.price:.2f} each"
+        return f"{self.name}  - ${self.price:.2f} each"
     
-    def total_price(self):
-        return self.price * self.quantity
+    
 
